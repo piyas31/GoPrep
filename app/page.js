559b6d25d00a6ef41cd44fa28809a9e83e107485
Piyas <piyasreza31@gmail.com>
2025-07-24 21:45:55 +0600

    setup convex db

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       </h2>
 
       <Button>Piyas</Button>
+      <UserButton/>
     </div>
 
   );
