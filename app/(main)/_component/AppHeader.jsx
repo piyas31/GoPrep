e3b2c122bc0b44d649db67f1b1217222d7c69ce8
Piyas <piyasreza31@gmail.com>
2025-08-07 17:13:04 +0600

    working on dashboard

--- app/(main)/_component/AppHeader.jsx
+++ app/(main)/_component/AppHeader.jsx
@@ -5,7 +5,7 @@ import { UserButton } from '@stackframe/stack'
 
 function AppHeader() {
   return (
-    <div>
+    <div className='p-3 shadow-sm flex justify-between items-center'>
         <Image src={'/logo.svg'} alt='logo'
         width={160}
         height={200}
